34aa7b53ed3943d07a4ab0331c362e76d2c7e42c
Sierk Hoeksma <sierk@hoeksma.org>
2018-01-04 15:10:36 +0100

    Fixed problems href from maps not moving location

--- js/_js/safariapp.js
+++ js/_js/safariapp.js
@@ -1,7 +1,6 @@
 (function(document,navigator,standalone) {
 // prevents links from apps from oppening in mobile safari
-// this javascript must be the first script in your <head>
-if (true || ((standalone in navigator) && navigator[standalone])) {
+if (((standalone in navigator) && navigator[standalone])) {
   var curnode, location=document.location, stop=/^(a|html)$/i;
   document.addEventListener('click', function(e) {
     curnode=e.target;
@@ -10,12 +9,14 @@ if (true || ((standalone in navigator) && navigator[standalone])) {
     }
     // Condidions to do this only on links to your own app
     // if you want all links, use if('href' in curnode) instead.
+    if (!('href' in curnode) && e.target && e.target.hasAttribute('href')) {
+      curnode=e.target;
+    }
     if('href' in curnode && // is a link
       (chref=curnode.href).replace(location.href,'').indexOf('#') && // is not an anchor
       (	!(/^[a-z\+\.\-]+:/i).test(chref) ||                       // either does not have a proper scheme (relative links)
         chref.indexOf(location.protocol+'//'+location.host)===0 ) // or is in the same protocol and domain
     ) {
-      alert(curnode.href);
       e.preventDefault();
       location.href = curnode.href;
     }
